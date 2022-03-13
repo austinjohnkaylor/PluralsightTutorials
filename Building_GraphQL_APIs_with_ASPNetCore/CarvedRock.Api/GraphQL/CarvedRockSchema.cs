899f82bd4661092b72395777c2edd7f78270f77f
Austin Kaylor <austin-kaylor@users.noreply.github.com>
2022-03-12 19:07:51 -0500

    Adding notes to the CarvedRockSchema to make sense of some of the objects used in there

--- Building_GraphQL_APIs_with_ASPNetCore/CarvedRock.Api/GraphQL/CarvedRockSchema.cs
+++ Building_GraphQL_APIs_with_ASPNetCore/CarvedRock.Api/GraphQL/CarvedRockSchema.cs
@@ -5,7 +5,7 @@ namespace CarvedRock.Api.GraphQL
 {
     public class CarvedRockSchema: Schema
     {
-        public CarvedRockSchema(IDependencyResolver resolver): base(resolver)
+        public CarvedRockSchema(IDependencyResolver resolver): base(resolver) // IDependencyResolver is an abstraction built on-top of whatever DI you are using. In this case, ASP.NET Core's DI container.
         {           
             Query = resolver.Resolve<CarvedRockQuery>();
         }
